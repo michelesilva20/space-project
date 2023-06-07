bc705da243182a33bca5197771f341c408cb7caf
michelesilva20 <michele.silva0511@gmail.com>
2023-06-07 20:07:59 -0300

    hero section finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,32 +21,68 @@ const Container = styled.div`
 
 const Left = styled.div`
   flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
 `;
+
 const Title = styled.h1`
-  
+  font-size: 74px;
 `;
+
 const WhatWeDo = styled.div`
-  
+  display: flex;
+  align-items: center;
+  gap:10px;
 `;
+
 const Line = styled.img`
-  
+  height: 5px;
 `;
+
 const Subtitle = styled.h2`
-  
+  color: #da4ea2;
 `;
+
 const Desc = styled.p`
-  
+  font-size: 24px;
+  color: lightgray;
 `;
 
 const Button = styled.button`
-  
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 `;
 
 const Right = styled.div`
-  flex: 8;
+  flex: 3;
+  position: relative;
 `;
-const Img = styled.img`
 
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate {
+    to{
+      transform: translateY(20px);
+    }
+  }
 `;
 
 const Hero = () => {
