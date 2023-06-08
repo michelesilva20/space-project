2a2dc34750c4e10b8e1f07b9c0ac1ab79d16ba58
michelesilva20 <michele.silva0511@gmail.com>
2023-06-08 15:11:22 -0300

    two day
    Feito section who
    Feito Section works
    Feito section contact
    Feito map da section contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,82 @@
 import React from 'react'
 import { styled } from 'styled-components'
+import Map from './Map'
 
 const Section = styled.div`
   height:100vh;
   scroll-snap-align: center;
-`
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 500px;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`;
+
+const Input = styled.input`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Textarea = styled.textarea`
+  padding: 20px;
+  background-color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: #fff;
+  border: none;
+  font-weight: bold;
+  border-radius: 5px;
+  padding: 20px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Contact = () => {
   return (
     <Section>
-      Contact
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input placeholder="Name"></Input>
+            <Input placeholder="Email"></Input>
+            <Textarea placeholder="Write your message" rows={10}/>
+            <Button>Send</Button>
+          </Form>
+        </Left>
+        <Right>
+          <Map/>
+        </Right>
+      </Container>
     </Section>
   )
 }
