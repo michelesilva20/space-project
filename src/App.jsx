99adeb21d0deb0cf5940d2bd14673c11a9db26fd
michelesilva20 <michele.silva0511@gmail.com>
2023-06-08 17:49:03 -0300

    three js
    Adicionado a biblioteca do threejs
    feito o cubo da section who

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import Who from "./components/Who"
 import Works from "./components/Works"
+import Test from "./components/Test"
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -24,6 +25,7 @@ function App() {
       <Who/>
       <Works/>
       <Contact/>
+      <Test/>
     </Container>
   )
 }
